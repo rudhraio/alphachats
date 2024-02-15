d5ad1924151d45855e26983437688aff78b685e3
rudhra <rudhra@panda.local>
2024-02-15 20:11:07 +0530

    feature: added getby userid

--- src/v1/chats/chats.get.js
+++ src/v1/chats/chats.get.js
@@ -3,15 +3,24 @@ import express from "express";
 import authorizeToken from "../../utils/middlewares/authorize.js";
 import logger from "../../utils/helpers/logger.js";
 import { notFoundResponse, serverErrorResponse, successResponse } from "../../utils/helpers/response.js";
-import { getChatsById, getChatsByUser } from "../../models/chats.model.js";
+import { getChatByMembers, getChatsById, getChatsByUser } from "../../models/chats.model.js";
 import { getMessagesByChatId } from "../../models/message.model.js";
 
 const chatsGet = express.Router();
 
 chatsGet.get("/", authorizeToken, async (req, res) => {
     try {
-        const chat = await getChatsByUser(req.user.id);
-        return successResponse(res, "chats list", { ...req.user, chat });
+        const { userid } = req.query;
+        console.log("userid", userid);
+        let chat = [], messages = [];
+        if (userid) {
+            chat = await getChatByMembers(req?.user?.id, userid);
+            messages = await getMessagesByChatId(chat.id);
+            chat = { ...chat, messages };
+        } else {
+            chat = await getChatsByUser(req.user.id);
+        }
+        return successResponse(res, "chats list", chat);
     } catch (error) {
         logger(`[ERR]: ${error}`, true);
         return serverErrorResponse(res);
